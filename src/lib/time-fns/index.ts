cdba8346cbc10158e2d56d71d60aca76d6051bbe
Anton Kosykh <kelin2025@yandex.ru>
2019-11-15 05:10:49 +0300

    raf -> setTimeout

--- src/lib/time-fns/index.ts
+++ src/lib/time-fns/index.ts
@@ -1,43 +1,43 @@
-import { createStore, createEvent } from "effector"
+import { createStore, createEvent } from "effector";
 
 export type Duration =
   | {
-      type: "seconds" | "minutes" | "hours"
-      value: number
+      type: "seconds" | "minutes" | "hours";
+      value: number;
     }
-  | number
+  | number;
 
 const timeMultipliers = {
   seconds: 1,
   minutes: 60,
-  hours: 3600,
-}
+  hours: 3600
+};
 
 export const formatTime = (time, format) => {
-  let hours = Math.floor(time / 1000 / 3600)
-  let minutes = Math.floor(time / 1000 / 60)
-  let seconds = Math.floor(time / 1000)
-  let miliseconds = Math.floor(time / 10)
+  let hours = Math.floor(time / 1000 / 3600);
+  let minutes = Math.floor(time / 1000 / 60);
+  let seconds = Math.floor(time / 1000);
+  let miliseconds = Math.floor(time / 10);
 
-  let fHours = Math.floor((time / 1000 / 3600) * 10) / 10
-  let fMinutes = Math.floor((time / 1000 / 60) * 10) / 10
-  let fSeconds = Math.floor((time / 1000) * 10) / 10
+  let fHours = Math.floor((time / 1000 / 3600) * 10) / 10;
+  let fMinutes = Math.floor((time / 1000 / 60) * 10) / 10;
+  let fSeconds = Math.floor((time / 1000) * 10) / 10;
 
-  const lowerFormat = format.toLowerCase()
-  const hasHours = lowerFormat.includes("{h}")
-  const hasMinutes = lowerFormat.includes("{m}")
-  const hasSeconds = lowerFormat.includes("{s}")
+  const lowerFormat = format.toLowerCase();
+  const hasHours = lowerFormat.includes("{h}");
+  const hasMinutes = lowerFormat.includes("{m}");
+  const hasSeconds = lowerFormat.includes("{s}");
 
   if (hasHours || hasMinutes || hasSeconds) {
-    miliseconds = miliseconds % 100
+    miliseconds = miliseconds % 100;
   }
 
   if (hasHours || hasMinutes) {
-    seconds = seconds % 60
+    seconds = seconds % 60;
   }
 
   if (hasHours) {
-    minutes = minutes % 60
+    minutes = minutes % 60;
   }
 
   return format
@@ -51,33 +51,33 @@ export const formatTime = (time, format) => {
     .replace("{h}", `${hours}`)
     .replace("{m}", `${minutes}`)
     .replace("{s}", `${seconds}`)
-    .replace("{ms}", `${miliseconds}`)
-}
+    .replace("{ms}", `${miliseconds}`);
+};
 
 export const intervalToSeconds = interval => {
   if (typeof interval === "object") {
-    const normalizedTime = +interval.value || 0
-    const multiplier = timeMultipliers[interval.type || "seconds"]
-    const convertedTime = normalizedTime * multiplier
+    const normalizedTime = +interval.value || 0;
+    const multiplier = timeMultipliers[interval.type || "seconds"];
+    const convertedTime = normalizedTime * multiplier;
 
-    return Number.isNaN(convertedTime) ? 0 : convertedTime
+    return Number.isNaN(convertedTime) ? 0 : convertedTime;
   }
   if (typeof +interval === "number" && !Number.isNaN(+interval)) {
-    return +interval
+    return +interval;
   }
-}
+};
 
-const dateTick = createEvent()
+const dateTick = createEvent();
 
 // NOTE: If this shit will lag
 // Replace with setInterval(dateTick, 1000)
 const rafTick = () => {
-  dateTick()
-  requestAnimationFrame(rafTick)
-}
+  dateTick();
+  setTimeout(rafTick, 50);
+};
 
-requestAnimationFrame(rafTick)
+setTimeout(rafTick, 50);
 
-export const $now = createStore<Date>(new Date())
+export const $now = createStore<Date>(new Date());
 
-$now.on(dateTick, () => new Date())
+$now.on(dateTick, () => new Date());
