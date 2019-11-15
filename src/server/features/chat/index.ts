972c34238b9a0ce96bc544a15ace1a908275cefa
Anton Kosykh <kelin2025@yandex.ru>
2019-11-15 07:04:05 +0300

    Added nanoid to chat messages

--- src/server/features/chat/index.ts
+++ src/server/features/chat/index.ts
@@ -1,9 +1,12 @@
+import nanoid from "nanoid";
 import { forward } from "effector";
 
 import { messageReceived } from "../../core/tmi";
 import { sendMessageEvent } from "./actions";
 
 forward({
-  from: messageReceived.map(options => ({ options })),
+  from: messageReceived.map(options => ({
+    options: { ...options, _id: nanoid() }
+  })),
   to: sendMessageEvent
 });
